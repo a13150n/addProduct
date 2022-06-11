be7cdd05dbe7c3709452c0a9150d870c84682106
abison <a13150n@gmail.com>
2022-06-12 00:48:57 +0530

    main ui completed

--- addproduct/src/App.js
+++ addproduct/src/App.js
@@ -3,6 +3,8 @@ import Main from "./component/Main";
 import Signup from "./component/Signup";
 import Login from "./component/Login";
 import Nav from './component/Nav';
+import Add from "./Add";
+import View from "./View";
 function App() {
 	const user = localStorage.getItem("token");
 	return (
@@ -11,6 +13,8 @@ function App() {
 		<Routes>
 			{user && <Route path="/" exact element={<Main />} />}
 			<Route path="/signup" exact element={<Signup />} />
+			<Route path="/add" exact element={<Add />} />
+			<Route path="/view" exact element={<View />} />
 			<Route path="/login" exact element={<Login />} />
 			<Route path="/" element={<Navigate replace to="/login" />} />
 		</Routes>
--- addproduct/src/component/Main.js
+++ addproduct/src/component/Main.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import './Main.css'
 
 function Main() {
   return (
-	<div>Main</div>
+
+	
+	<div className="main_container">
+			<div className="signup_form_container">
+				
+				<div className="right1">
+					<h1>ADD NEW PRODUCT & DETAILS</h1>
+					<Link to="/add">
+						<button type="button" className="green_btn1">
+							ADD
+						</button>
+					</Link>
+				</div>
+				<div className="left1">
+				<h1>VIEW ALL PRODUCTS & DETAILS</h1>
+					<Link to="/view">
+						<button type="button" className="white_btn1">
+							VIEW
+						</button>
+					</Link>
+				</div>
+			</div>
+		</div>
+		
   )
 }
 
