f35abff0fe5c8fdb995af15dd59b9ce76709fe68
abison <a13150n@gmail.com>
2022-06-11 14:43:44 +0530

    .......set routes .......

--- authentication/server/index.js
+++ authentication/server/index.js
@@ -3,11 +3,17 @@ const express = require('express');
 const app = express();
 const cors =require("cors");
 const connection = require("./db");
+const userRoutes = require('./routes/user');
+const authRoutes = require('./routes/auth')
+
 
 connection();
 
 app.use(express.json())
 app.use(cors())
+
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 const port =process.env.PORT||8080;
 app.listen(port, () =>console.log(`listening at ${port}..`))
